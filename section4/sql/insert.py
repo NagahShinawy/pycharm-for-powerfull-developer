923a76c71c4818257a3e8ea30eb20825a3b25f1b
Nagah Shinawy <E-n.Shinawy@lean.sa>
2021-04-19 15:11:56 +0300

    using insert

---
+++ section4/sql/insert.py
@@ -0,0 +1,28 @@
+
+"""
+created by Nagaj at 19/04/2021
+"""
+
+import os
+import sqlite3
+
+import pandas as pd
+
+connection = sqlite3.connect(os.path.join(os.getcwd(), "menu.db"))
+cursor = connection.cursor()
+
+menus = [
+    ("white", 4, 5),
+    ("white", 7, 9.8),
+
+]
+cursor.executemany("INSERT INTO menu(bread, charge, cost) values (?,?,?)", menus)
+
+connection.commit()
+
+menu_df = pd.read_sql_query("SELECT bread, charge, cost from menu WHERE charge = 4", connection)
+
+
+print(menu_df)
+
+connection.close()
